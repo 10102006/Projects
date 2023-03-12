93a7b87d2947a4b9b6b5fcf987958be08042dcd1
Udit Kumar <uditknishad2119@gmail.com>
2023-03-12 12:46:50 +0530

    flow in now function

--- My Day/flow.py
+++ My Day/flow.py
@@ -1,11 +1,15 @@
 """
-STATUS:
-OVERVIEW:
+STATUS: Working
+OVERVIEW: controls the flow of the events happening
 IMPROVEMENTS:
-Workings:
+    - better `append()`
+    - make it more streamlined, meaning not broken but to work as a single unit
+    - implement preiod system
 """
 
 # @ Imports
+import pprint
+pp = pprint.PrettyPrinter(indent=4, width=1).pprint
 
 # * Defining
 
@@ -17,27 +21,35 @@ class Flow():
             ('study', '16:00'): [],
             ('sleep', '23:00'): []
         }
-        self.events = events
+        self.events = list(events)
+
+        self.sync()
+
+    def sync(self, events=None):
+        """ """
+        if events:
+            self.events = events
 
         self.initialise()
-        self.sync()
+        self.append()
         self.reiterate()
 
-        print(self.flow)
-
-    def sync(self):
+    def append(self):
         """ """
         counter = 0
-        print(len(self.events))
-        while counter < 100:
+        while counter < max((len(events) for events in self.flow.values())):
             for keyAnchor in self.flow.keys():
+
+                if len(self.flow[keyAnchor]) <= counter:
+                    continue
+
                 for event in (self.events):
-                    try:
-                        if event.anchor == self.flow[keyAnchor][counter].name:
-                            self.flow[keyAnchor].append(event)
-                            self.events.remove(event)
-                    except:
-                        pass
+                    newAnchor = self.flow[keyAnchor][counter]
+
+                    if event.anchor == newAnchor.name:
+                        self.flow[keyAnchor].append(event)
+                        self.events.remove(event)
+
             counter += 1
 
     def initialise(self):
@@ -51,23 +63,30 @@ class Flow():
     def reiterate(self):
         """ """
         for event in self.events:
-            self.flow[('', event.anchor)] = [event]
+            if ':' in event.anchor:
+                self.flow[('', event.anchor)] = [event]
 
 
 class Event():
-    def __init__(self, name, anchor, period):
+    def __init__(self, name, anchor, period=''):
         self.name = name
         self.anchor = anchor
-        self.period = period
+        # self.period = period
+
+    def get(self):
+        return self.__dict__
 
 
 # ? Implementation
 evnts = [
-    Event('exercise', 'wake up', 30),
-    Event('draw', 'exercise', 30),
-    Event('brush', '22:00', 10),
-    Event('code', 'study', 120)
+    Event('exercise', 'wake up'),
+    Event('draw', 'exercise'),
+    Event('brush', '22:00'),
+    Event('code', 'study'),
+    Event('jornal', 'brush'),
+    Event('draw', 'jornal')
 ]
 
 if __name__ == "__main__":
     f = Flow(evnts)
+    pp(f.flow)
