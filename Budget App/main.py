57fca60cbc4b2e1c62f34e6fa9e29867499b66d5
Udit Kumar <uditknishad2119@gmail.com>
2023-04-22 14:37:33 +0530

    Documented

--- Budget App/main.py
+++ Budget App/main.py
@@ -1,6 +1,8 @@
 """
-STATUS:
-OVERVIEW:
+STATUS: Working
+
+OVERVIEW: A basic program to handle money withdrawing, deposit and transfer btwn accounts
+
 IMPROVE:
     - Use decorator for CheckFunds()
     - Make a ledger class
@@ -58,12 +60,11 @@ class Category:
                 f"{description[:23]}{amount[:7]}")
         print(f"Total: {self.GetBalance()}")
 
-# READ
+
 class Chart:
     def __init__(self, categories):
         self.categories = categories
         self.FindSpentPercent()
-        # self.View()
 
     def FindSpentPercent(self):
         """ Will find how much spending each category has done based on ledger. """
@@ -74,73 +75,27 @@ class Chart:
             totalSpent = 0
 
             for entry in category.ledger:
-                if entry['amount'] < 0: # means a expenditure
+                if entry['amount'] < 0:  # means a expenditure
                     totalSpent += entry['amount']
                     grandTotalSpent += entry['amount']
 
             self.percentTable.update({category.categoryName: totalSpent})
 
         for category, amount in self.percentTable.items():
-            self.percentTable[category] = round(
-                (amount/grandTotalSpent) * 10, 0)
-
+            self.percentTable[category] = int(round(
+                (amount/grandTotalSpent) * 10, 0))
 
     def Graph(self):
         """ """
-        keys = ['']
+        print(self.percentTable)
 
-        # Makes a 11 X 5 Graph
-        graph = [
-            [' ' for _ in range(5)]
-                 for _ in range(11)
-        ]
+        # Header
+        print(' ' * 10, *[i * 10 for i in range(1, 11)])
+        print(' ' * 10, '-' * 30)
 
-        for ri, row in enumerate(graph):
-            for ci, pi in enumerate(self.percentTable.values(), start=1):
-                if 9-pi < ri:
-                    graph[ri][ci] = 'o'
-            graph[ri][0] = f"{(10-ri) * 10} |".rjust(5)
-
-        for key in self.percentTable.keys():
-            vl = ''
-            for char in key:
-                vl = vl + f"\n{char}"
-            keys.append(vl)
-
-        for row in graph:
-            print(*row)
-
-    def WordOrient(self):
-        """ """
-        keys = list(self.percentTable.keys())
-
-        longest = len(keys[0])
-        for key in keys:
-            if len(key) > longest:
-                longest = len((key))
-
-        tb = [['-' for _ in keys]]
-        for _ in range(longest):
-            tb.append(['' for _ in keys])
-
-        for ri, row in enumerate(tb):
-            for ci, key in enumerate(row):
-                try:
-                    char = keys[ci][ri]
-                except:
-                    pass
-                else:
-                    pass
-                    tb[ri+1][ci] = char
-
-        for i in tb:
-            print(' ' * 5, *i)
-
-    def View(self):
-        """ """
-        print("Percent Spent by Category")
-        self.Graph()
-        self.WordOrient()
+        for category, percent in self.percentTable.items():
+            categoryName = category.ljust(8)
+            print(categoryName, '|', *['O ' for _ in range(percent)])
 
 
 # ? Implementation
@@ -158,14 +113,3 @@ if __name__ == "__main__":
     cloth.Transfer(food, 550)
     chart = Chart([food, cloth])
     chart.Graph()
-
-"""
-    food.PrintData()
-    cloth.PrintData()
-
-    room = Category('ROOM')
-    room.Deposit(2280, 'Initial Deposit')
-    room.Withdraw(450, 'Pizza sauce bought')
-    room.Withdraw(450, 'Pizza sauce bought')
-    cloth.Transfer(room, 100)
-"""
