e7b4a0f542464a0813209ac5b2a0fe63a5f0d4d4
Udit Kumar <uditknishad2119@gmail.com>
2023-04-22 14:24:47 +0530

    Vertical Graph

--- Budget App/main.py
+++ Budget App/main.py
@@ -1,7 +1,9 @@
 """
 STATUS:
 OVERVIEW:
-IMPROVEMENTS:
+IMPROVE:
+    - Use decorator for CheckFunds()
+    - Make a ledger class
 Workings:
 """
 
@@ -14,30 +16,22 @@ class Category:
     def __init__(self, name):
         """ So this functions makes a class a constructor """
         self.categoryName = name.capitalize()
-        #  {"amount": amount, "description": description} ledger obj
-        self.ledger = []
+
+        self.ledger = [] # data = {"amount": amount, "description": description}
 
     def Deposit(self, amount, description=''):
         """ For deposting in the ledger. """
-        self.ledger.append({"amount": amount, "description": description})
+        self.ledger.append({"amount": +amount, "description": description})
 
     def Withdraw(self, amount, description=''):
-        """For withdrawing from the ledger. """
+        """ For withdrawing from the ledger. """
         if self.CheckFunds(amount):
             self.ledger.append({"amount": -amount, "description": description})
 
     def GetBalance(self):
-        """ Final amount present. """
-        balances = []
-        totalAmt = 0
-
-        for entry in self.ledger:
-            balances.append(entry['amount'])
-
-        for amt in balances:
-            totalAmt += amt
-
-        return totalAmt
+        """ For presenting the final amount. """
+        balances = [entry['amount'] for entry in self.ledger]
+        return sum(balances)
 
     def Transfer(self, category, amount):
         """ Transfer amt from one category instance to another. """
@@ -54,44 +48,52 @@ class Category:
             return False
 
     def PrintData(self):
-        """ Prints category info in a systemised format. """
-        # a = ''.ljust()
+        """ Prints category info in a systematized format. """
         print(self.categoryName.center(30, '*'))
         for entry in self.ledger:
+            # IMPROVE
+            description = entry['description'].ljust(23)
+            amount = str(entry['amount']).rjust(7)
             print(
-                f"{entry['description'].ljust(23)[:23]}{str(entry['amount']).rjust(7)[:7]}")
+                f"{description[:23]}{amount[:7]}")
         print(f"Total: {self.GetBalance()}")
 
-
+# READ
 class Chart:
     def __init__(self, categories):
-        """ So this functions makes a class a constructor """
         self.categories = categories
-        self.FindPercent()
-        self.View()
+        self.FindSpentPercent()
+        # self.View()
 
-    def FindPercent(self):
-        """ Will find the percent based on ledger. """
+    def FindSpentPercent(self):
+        """ Will find how much spending each category has done based on ledger. """
         self.percentTable = {}
-        grandTotal = 0
+        grandTotalSpent = 0
+
         for category in self.categories:
             totalSpent = 0
+
             for entry in category.ledger:
-                if entry['amount'] < 0:
-                    totalSpent -= entry['amount']
-                    grandTotal -= entry['amount']
+                if entry['amount'] < 0: # means a expenditure
+                    totalSpent += entry['amount']
+                    grandTotalSpent += entry['amount']
+
             self.percentTable.update({category.categoryName: totalSpent})
 
-        for val in self.percentTable:
-            self.percentTable[val] = int(round(
-                (self.percentTable[val]/grandTotal) * 10, 0))
+        for category, amount in self.percentTable.items():
+            self.percentTable[category] = round(
+                (amount/grandTotalSpent) * 10, 0)
+
 
     def Graph(self):
         """ """
         keys = ['']
-        graph = []
-        for pi in range(11):
-            graph.append([' ' for _ in range(5)])
+
+        # Makes a 11 X 5 Graph
+        graph = [
+            [' ' for _ in range(5)]
+                 for _ in range(11)
+        ]
 
         for ri, row in enumerate(graph):
             for ci, pi in enumerate(self.percentTable.values(), start=1):
@@ -145,27 +147,25 @@ class Chart:
 if __name__ == "__main__":
     food = Category('food')
     cloth = Category('clotheS')
-    room = Category('ROOM')
+
     food.Deposit(1750, 'Initial Deposit')
-    room.Deposit(2280, 'Initial Deposit')
     cloth.Deposit(2510, 'Initial Deposit')
 
-    food.Withdraw(150, 'Pizza sauce bought')
-    room.Withdraw(450, 'Pizza sauce bought')
-    room.Withdraw(450, 'Pizza sauce bought')
-    cloth.Withdraw(520, 'Pizza sauce bought')
-    food.Transfer(cloth, 300)
-    food.Transfer(cloth, 300)
-    food.Transfer(cloth, 300)
-    food.Transfer(cloth, 300)
-    food.Transfer(cloth, 300)
-    cloth.Transfer(room, 100)
+    food.Withdraw(150, 'Pizza bought')
+    cloth.Withdraw(520, 'Pizza hat bought')
 
+    food.Transfer(cloth, 300)
+    cloth.Transfer(food, 550)
+    chart = Chart([food, cloth])
+    chart.Graph()
+
+"""
     food.PrintData()
-    print()
-    room.PrintData()
-    print()
     cloth.PrintData()
-    print()
 
-    chart = Chart([food, room, cloth])
+    room = Category('ROOM')
+    room.Deposit(2280, 'Initial Deposit')
+    room.Withdraw(450, 'Pizza sauce bought')
+    room.Withdraw(450, 'Pizza sauce bought')
+    cloth.Transfer(room, 100)
+"""
